61e8f76e9a8f5d1b80b6168d0c33bf44728a781d
luis <luiscarvajal2693@gmail.com>
2018-12-20 00:05:36 -0500

    links y fixes

--- js/util.js
+++ js/util.js
@@ -13,6 +13,21 @@ function goTo(section) {
     });
 }
 
+function goToCustom(section) {
+    var cfg = {
+        scrollDuration : 800
+    };
+
+    var target = this.hash,
+        $target    = $('#'+section);
+
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top - 90
+        // 'scrollTop': $target.offset().top
+    }, cfg.scrollDuration, 'swing', function () {
+    });
+}
+
 function checkIfMobile() {
     return $('html body').width() <= 768;
 }
