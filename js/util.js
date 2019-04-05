219345c6930c5e960d329d1e2140e052fd14b35c
luis <luiscarvajal2693@gmail.com>
2019-04-05 16:00:42 -0400

    logica de hover en proyectos

--- js/util.js
+++ js/util.js
@@ -39,3 +39,49 @@ function checkNavegation() {
         }
     }
 }
+
+let bannerTimeOut = null;
+let bannerArray = [];
+let isVideo = false;
+
+function startTimeOut(){
+    if (isVideo) {
+        $( "#videoPreview" ).hover(
+            function() {
+                clearTimeout(bannerTimeOut);
+                showBanner();
+            }, function() {
+                bannerTimeOut = setTimeout(function(){
+                    hideBanner();
+                }, 5000);
+            }
+        );
+    } else {
+        $( "#carousel-example-generic" ).hover(
+            function() {
+                clearTimeout(bannerTimeOut);
+                showBanner();
+            }, function() {
+                bannerTimeOut = setTimeout(function(){
+                    hideBanner();
+                }, 5000);
+            }
+        );
+    }
+
+    bannerTimeOut = setTimeout(function(){
+        hideBanner();
+    }, 15000);
+}
+
+function hideBanner() {
+    bannerArray.forEach(number => {
+        $('#banner'+number).css('display', 'none');
+    });
+}
+
+function showBanner() {
+    bannerArray.forEach(number => {
+        $('#banner'+number).css('display', 'block');
+    });
+}
