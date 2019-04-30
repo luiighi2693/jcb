b387ef85a9456f42fdbfee9c0cf47d8a5e8a451d
luis <luiscarvajal2693@gmail.com>
2019-04-30 11:04:36 -0400

    intro y nosotros responsive

--- js/util.js
+++ js/util.js
@@ -43,6 +43,7 @@ function checkNavegation() {
 let bannerTimeOut = null;
 let bannerArray = [];
 let isVideo = false;
+let screeenType = '';
 
 function startTimeOut(){
     if (isVideo) {
@@ -84,4 +85,57 @@ function showBanner() {
     bannerArray.forEach(number => {
         $('#banner'+number).css('display', 'block');
     });
+}
+
+function processMobile() {
+
+    if ($('html body').height() > $('html body').width()) { //mobile
+        console.log('mobile');
+
+        $('#header-nav-wrap').height($('html body').height());
+
+        if (screeenType === 'intro'){
+            $('#footer-main-block').height($('html body').height() - 40 - $('#videoPreview').height());
+
+            $('#footer-row-responsive-block')
+                .css('position', 'absolute')
+                .css('top', '50%')
+                .css('left', '50%')
+                .css('transform', 'translate(-50%, -50%)')
+                .css('-ms-transform', 'translate(-50%, -50%)');
+        } else {
+            $('#footer-main-block').css('height', 'auto');
+
+            $('#footer-row-responsive-block')
+                .css('position', 'relative')
+                .css('top', '0')
+                .css('left', '0')
+                .css('transform', 'none')
+                .css('-ms-transform', 'none');
+        }
+
+
+
+    } else { // tablet
+        console.log('tablet');
+        $('#footer-main-block').css('height', 'auto');
+
+        $('#footer-row-responsive-block')
+            .css('position', 'relative')
+            .css('top', '0')
+            .css('left', '0')
+            .css('transform', 'none')
+            .css('-ms-transform', 'none');
+    }
+}
+
+function processWeb() {
+    $('#footer-main-block').css('height', 'auto');
+
+    $('#footer-row-responsive-block')
+        .css('position', 'relative')
+        .css('top', '0')
+        .css('left', '0')
+        .css('transform', 'none')
+        .css('-ms-transform', 'none');
 }
